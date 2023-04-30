a28d173d1ec7184cd5971bce480133c42f7913b3
Fartein <fartein.sveindal@shifthyperloop.com>
2023-04-30 18:05:05 +0200

    put iris in folder

---
+++ Iris_project/calculateErrorRate.m
@@ -0,0 +1,9 @@
+    function errorRate = calculateErrorRate(confusionMatrix,testPerClass)
+    C = length(confusionMatrix);
+    incorrectPred = 0;
+    for i = 1:C
+        incorrectPred = incorrectPred + (testPerClass - confusionMatrix(i, i));
+    end
+    errorRate = incorrectPred/(testPerClass*C);
+end
+
